db139b84ada44ef8ff8a4950523ed4ebde087b75
josemartin81 <jose_etsi@hotmail.com>
2015-12-22 13:40:11 +0100

    Homework 1

---
+++ RestaurantManager/RestaurantManager.Models/ExpediteDataManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantManager.Models
+{
+    public class ExpediteDataManager : DataManager
+    {
+
+        private List<Order> _OrderItems;
+
+        protected override void OnDataLoaded()
+        {
+            this.OrderItems = base.Repository.Orders;
+        }
+
+        public List<Order> OrderItems
+        {
+            get { return this._OrderItems ; }
+            set
+            {
+                if (value != this._OrderItems)
+                {
+                    this._OrderItems = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
---
+++ RestaurantManager/RestaurantManager.Models/OrderDataManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantManager.Models
+{
+    public class OrderDataManager : DataManager
+    {
+
+        private List<MenuItem> _MenuItems;
+        private List<MenuItem> _CurrentlySelectedMenuItem;
+
+
+        protected override void OnDataLoaded()
+        {
+            this.MenuItems = base.Repository.StandardMenuItems;
+
+            this.CurrentlySelectedMenuItems = new List<MenuItem>
+            {
+                this.MenuItems[3],
+                this.MenuItems[5]
+            };
+        }
+
+        public List<MenuItem> MenuItems {
+            get { return this._MenuItems; }
+            set
+            {
+                if (value != this._MenuItems )
+                {
+                    this._MenuItems = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+        public List<MenuItem> CurrentlySelectedMenuItems {
+            get { return this._CurrentlySelectedMenuItem; }
+            set
+            {
+                if (value != this._CurrentlySelectedMenuItem)
+                {
+                    this._CurrentlySelectedMenuItem = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
