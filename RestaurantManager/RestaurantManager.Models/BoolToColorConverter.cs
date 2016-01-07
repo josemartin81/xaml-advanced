732c095cdabed6377bb94b386a2145bc330616cd
josemartin81 <jose_etsi@hotmail.com>
2016-01-07 09:40:36 +0100

    Homework #3: Converters &  Behaviors

---
+++ RestaurantManager/RestaurantManager.Models/BoolToColorConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace RestaurantManager.Models
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+
+    public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var b = value as bool?;
+
+            if (b.HasValue)
+            {
+                return (b.Value) ? new SolidColorBrush(Colors.Red) : new SolidColorBrush(Colors.Transparent);
+            }
+
+            return new SolidColorBrush(Colors.Transparent);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            var c = value as SolidColorBrush;
+
+            if (c != null)
+            {
+                return c.Color  == Colors.Red;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ RestaurantManager/RestaurantManager.Models/RightClickMessageDialogBehavior.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace RestaurantManager.Models
+{
+    public class RightClickMessageDialogBehavior : DependencyObject, IBehavior
+    {
+        public String Title { get; set; }
+        public String Message { get; set; }
+
+        public DependencyObject AssociatedObject { get; private set; }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            AssociatedObject = associatedObject;
+            if (AssociatedObject is Page)
+            {
+                (AssociatedObject as Page).RightTapped += RightClickMessageDialogBehavior_RightTapped;
+            }
+        }
+
+        private async void RightClickMessageDialogBehavior_RightTapped(object sender, Windows.UI.Xaml.Input.RightTappedRoutedEventArgs e)
+        {
+            await new MessageDialog(Message, Title).ShowAsync();
+        }
+
+        public void Detach()
+        {
+            if (AssociatedObject is Page)
+            {
+                (AssociatedObject as Page).RightTapped -= RightClickMessageDialogBehavior_RightTapped;
+            }
+        }
+    }
+}
